9d97fbb61b4305e0ce8407144b93855c76de0239
Keane Viktor <scren2010@gmail.com>
2020-07-19 22:50:04 +0600

    add urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,7 @@ from profiles import views
 urlpatterns = [
     path('', views.ProfileView.as_view()),
     path('public-info/', views.PublicUserInfo.as_view()),
+    path('update-ava/', views.UpdateProfile.as_view()),
+    path('update-nike/', views.UpdateNike.as_view()),
+    path('follow/', views.AddFollow.as_view()),
 ]
